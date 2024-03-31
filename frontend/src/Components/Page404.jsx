a03b03d2e0e6ae53fd45f9375c72ffc6000c9b59
bril95 <bril.vadim@mail.ru>
2024-03-31 12:49:38 +0300

    created 2 routers

---
+++ frontend/src/Components/Page404.jsx
@@ -0,0 +1,9 @@
+const page404 = () => {
+  return (
+    <>
+      <h3>Страница не найдена</h3>
+    </>
+  )
+};
+
+export default page404;
---
+++ frontend/src/Components/PageLogin.jsx
@@ -0,0 +1,9 @@
+const login = () => {
+  return (
+    <>
+      <h3>Страница c логином</h3>
+    </>
+  )
+};
+
+export default login;
