19d4b19e4c3a3da07b9017ada81c038e8c9f6b76
bril95 <bril.vadim@mail.ru>
2024-05-13 11:16:04 +0300

    fix 'SignUpPage'

--- frontend/src/Components/SignUpPage.jsx
+++ frontend/src/Components/SignUpPage.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setCurrentAuthor } from '../Slice/currentAuthorSlice';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'; 
 
 const SignUp = () => {
   const [addUser] = useAddUserMutation();
@@ -52,7 +52,7 @@ const SignUp = () => {
       setError(t('signUpPage.errorRegistration'));
     }
   };
-  
+
   return (
     <div className="h-100 bg-light">
       <div className="h-100">
@@ -63,64 +63,62 @@ const SignUp = () => {
                 <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
               </Container>
             </Navbar>
-            <Container fluid className="h-100">
-              <div className="row justify-content-center align-content-center h-100">
-                <div className="col-12 col-md-8 col-xxl-6">
-                  <Card className="shadow-sm">
-                    <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-                      <div><img src="/Registration.jpg" className="rounded-circle" alt="Регистрация" /></div>
-                      <Formik
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={handleSubmit}
-                      >
-                        {({ isSubmitting }) => (
-                          <Form className="w-50" noValidate>
-                            <h1 className="text-center mb-4">{t('signUpPage.registration')}</h1>
-                            <div className="form-floating mb-3">
-                              <Field
-                                type="text"
-                                name="username"
-                                id="username"
-                                placeholder={t('signUpPage.placeholder.min3max20')}
-                                className="form-control"
-                              />
-                              <label htmlFor="username">{t('signUpPage.username')}</label>
-                              <ErrorMessage name="username" component="div" className="invalid-tooltip" />
-                            </div>
-                            <div className="form-floating mb-3">
-                              <Field
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder={t('signUpPage.placeholder.min6')}
-                                className="form-control"
-                              />
-                              <label htmlFor="password">{t('signUpPage.password')}</label>
-                              <ErrorMessage name="password" component="div" className="invalid-tooltip" />
-                            </div>
-                            <div className="form-floating mb-4">
-                              <Field
-                                type="password"
-                                name="confirmPassword"
-                                id="confirmPassword"
-                                placeholder={t('signUpPage.placeholder.samePassword')}
-                                className="form-control"
-                              />
-                              <label htmlFor="confirmPassword">{t('signUpPage.confirmPassword')}</label>
-                              <ErrorMessage name="confirmPassword" component="div" className="invalid-tooltip" />
-                            </div>
-                            <Button type="submit" disabled={isSubmitting} className="w-100 btn btn-primary">
-                            {t('signUpPage.signUp')}
-                            </Button>
-                            {error && <div className="text-danger mt-2">{error}</div>}
-                          </Form>
-                        )}
-                      </Formik>
-                    </Card.Body>
-                  </Card>
-                </div>
-              </div>
+            <Container fluid className="h-100 d-flex justify-content-center align-items-center">
+              <Card className="shadow-sm">
+              <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+                  <div>
+                    <img src="/Registration.jpg"className="rounded-circle" alt="Регистрация" />
+                  </div>
+                  <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                  >
+                    {({ isSubmitting }) => (
+                      <Form>
+                        <h1 className="text-center mb-4">{t('signUpPage.registration')}</h1>
+                        <div className="form-floating mb-3">
+                          <Field
+                            type="text"
+                            name="username"
+                            id="username"
+                            placeholder={t('signUpPage.placeholder.min3max20')}
+                            className="form-control"
+                          />
+                          <label htmlFor="username">{t('signUpPage.username')}</label>
+                          <ErrorMessage name="username" component="div" className="text-danger" />
+                        </div>
+                        <div className="form-floating mb-3">
+                          <Field
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder={t('signUpPage.placeholder.min6')}
+                            className="form-control"
+                          />
+                          <label htmlFor="password">{t('signUpPage.password')}</label>
+                          <ErrorMessage name="password" component="div" className="text-danger" />
+                        </div>
+                        <div className="form-floating mb-4">
+                          <Field
+                            type="password"
+                            name="confirmPassword"
+                            id="confirmPassword"
+                            placeholder={t('signUpPage.placeholder.samePassword')}
+                            className="form-control"
+                          />
+                          <label htmlFor="confirmPassword">{t('signUpPage.confirmPassword')}</label>
+                          <ErrorMessage name="confirmPassword" component="div" className="text-danger" />
+                        </div>
+                        <Button type="submit" disabled={isSubmitting} className="w-100 btn">
+                        {t('signUpPage.signUp')}
+                        </Button>
+                        {error && <div className="text-danger mt-2">{error}</div>}
+                      </Form>
+                    )}
+                  </Formik>
+                </Card.Body>
+              </Card>
             </Container>
           </div>
         </div>
@@ -128,5 +126,5 @@ const SignUp = () => {
     </div>
   );
 };
-
+  
 export default SignUp;
