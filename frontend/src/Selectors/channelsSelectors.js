06f0359fa02b4f8289bf6913f4006396a2172dd6
bril95 <bril.vadim@mail.ru>
2024-06-09 13:58:41 +0300

    fix linter

--- frontend/src/Selectors/channelsSelectors.js
+++ frontend/src/Selectors/channelsSelectors.js
@@ -4,10 +4,10 @@ const channelsStore = (state) => state.channels.channels;
 
 export const selectDefaultChannel = createSelector(
   channelsStore,
-  (channels) => channels.find((channel) => channel.name === 'general')
+  (channels) => channels.find((channel) => channel.name === 'general'),
 );
 
 export const selectChannelsName = createSelector(
   channelsStore,
-  (channels) => channels ? channels.map((channel) => channel.name) : []
+  (channels) => channels ? channels.map((channel) => channel.name) : [],
 );
